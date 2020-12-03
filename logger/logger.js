5ad33782f20f2aab995e10fbff4acfc51847abeb
Ranbir7925 <honeyranbir7925@gmail.com>
2020-12-03 21:12:24 +0530

    add:Code for winstin logger

---
+++ logger/logger.js
@@ -0,0 +1,16 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+    levels: winston.config.syslog.levels,
+    transports: [
+        new winston.transports.File({
+            filename: './logs/errors.log',
+            level: 'error'
+        }),
+        new winston.transports.File({
+            filename: './logs/info.log',
+            level: 'info'
+        })
+    ]
+});
+module.exports = logger;
